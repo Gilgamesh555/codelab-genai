4b2eba0a825b672536e1b0269c28313b7c8c4f95
Gilgamesh555 <painarniano@gmail.com>
2024-09-13 17:59:03 +0000

    add-latest-changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    template: `
+        <section>
+            <ol>
+                @for(fact of facts(); track fact) {
+                    <li>{{fact}}</li>  
+                } @empty {
+                    <li>No facts are available</li>
+                }
+            </ol>
+        </section>
+    `,
+    styles: '',
 })
 export class AppComponent {
-  title = 'codelab-genai';
+    title = 'ssr-xp';
+    facts = signal<string[]>([]);
+
+    constructor() {
+        fetch('/api/facts').then(response => response.json()).then(facts => {
+        this.facts.set(facts);
+        });
+    }
 }
